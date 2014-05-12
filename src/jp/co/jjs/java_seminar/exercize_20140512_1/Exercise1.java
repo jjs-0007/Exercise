0533e071947d2dab2824f0b4594e7eaa4fe0936d
jjs0007 <jjs-0007@outlook.jp>
2014-05-12 15:50:46 +0900

    test

---
+++ src/jp/co/jjs/java_seminar/exercize_20140512_1/Exercise1.java
@@ -0,0 +1,24 @@
+package jp.co.jjs.java_seminar.exercize_20140512_1;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		int a	  = 1; 
+		float b   = 2.0f;
+		boolean c = true;
+		double d  = 4.9;
+		long e	  = 5;
+		short f   = 6;
+		byte g    = 7;
+		char h    = 'h';
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+	}
+
+}
