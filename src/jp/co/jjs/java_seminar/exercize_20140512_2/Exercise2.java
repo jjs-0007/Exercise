d2cfcf776922001dd34ef3ded66b196b78b660f1
jjs0007 <jjs-0007@outlook.jp>
2014-05-12 16:13:16 +0900

    test2

---
+++ src/jp/co/jjs/java_seminar/exercize_20140512_2/Exercise2.java
@@ -0,0 +1,11 @@
+package jp.co.jjs.java_seminar.exercize_20140512_2;
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		int kion  = 24;
+		String tenki = "曇り";
+		
+		System.out.println("今日の札幌の天気は" + tenki + "で、気温は" + kion + "℃です。");
+	}
+
+}
