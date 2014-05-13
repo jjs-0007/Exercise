850639a1a64233ab3223eda77509b4ca684b861a
jjs0007 <jjs-0007@outlook.jp>
2014-05-13 11:02:04 +0900

    test

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_02/Exercise4.java
@@ -0,0 +1,19 @@
+package jp.co.jjs.java_seminar.exercise_20140513_02;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        short x;
+        short y;
+
+        x = 1;
+        y = 2;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        int z = x + y;
+        System.out.println(z);
+
+    }
+
+}
