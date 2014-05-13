55fda6d3284e63568e22e080e95aafbf22c1f7ae
jjs0007 <jjs-0007@outlook.jp>
2014-05-13 13:21:02 +0900

    ex6,7

--- src/jp/co/jjs/java_seminar/exercise_20140513_04/Exercise6.java
+++ src/jp/co/jjs/java_seminar/exercise_20140513_04/Exercise6.java
@@ -2,8 +2,7 @@ package jp.co.jjs.java_seminar.exercise_20140513_04;
 
 public class Exercise6 {
     public static void main(String[] args) {
-        int n;
-        n = 9;
+        int n = 9;
         
         if(n % 2 == 0) {
             System.out.println("偶数");
