34b0a6834f3c6e8b484aa31ba933f9679640753f
jjs0007 <jjs-0007@outlook.jp>
2014-05-13 15:03:17 +0900

    ex9

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_07/Exercise9.java
@@ -0,0 +1,32 @@
+package jp.co.jjs.java_seminar.exercise_20140513_07;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        int[] x;
+        
+        x = new int[2];
+        x[0] = 10;
+        x[1] = 20;
+        
+        int[] y = {1,2,3,4};
+        
+        System.out.println(x.length);
+        System.out.println(y.length);
+        
+        
+        String[] a;
+        
+        a = new String[4];
+        a[0] = "abc";
+        a[1] = "bcd";
+        a[2] = "cde";
+        a[3] = "def";
+        
+        String[] b = {"a","b","c","d"};
+       
+        System.out.println(a.length);
+        System.out.println(b.length);
+        
+    }
+
+}
