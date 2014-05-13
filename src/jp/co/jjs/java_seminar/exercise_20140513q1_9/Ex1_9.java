2c06d89791844ec301d607ebfdff93aca322749b
jjs0007 <jjs-0007@outlook.jp>
2014-05-13 16:41:16 +0900

    ex1-9

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513q1_9/Ex1_9.java
@@ -0,0 +1,10 @@
+package jp.co.jjs.java_seminar.exercise_20140513q1_9;
+
+public class Ex1_9 {
+    public static void main(String[] args) {
+        int num = 1000;
+        
+        System.out.println(num + " is the value of num");
+    }
+
+}
