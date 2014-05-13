279a1e1c9682291359761c98d0464056c94ab780
jjs0007 <jjs-0007@outlook.jp>
2014-05-13 10:38:10 +0900

    test

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_01/Exercise3.java
@@ -0,0 +1,18 @@
+package jp.co.jjs.java_seminar.exercise_20140513_01;
+
+public class Exercise3 {
+	public static void main(String[] args) {
+		int x;
+		int y;
+		
+		x = 10;
+		y = 3;
+		
+		System.out.println(x + "+" + y + "=" + (x + y));
+		System.out.println(x + "-" + y + "=" + (x - y));
+		System.out.println(x + "÷" + y + "=" + (x / (float)y));
+		System.out.println(x + "×" + y + "=" + (x * y));
+		System.out.println(x + "%" + y + "=" + (x % y));
+	}
+
+}
