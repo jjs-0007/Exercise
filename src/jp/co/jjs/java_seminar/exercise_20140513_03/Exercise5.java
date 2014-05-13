bda60a04302742015fc9fc4772647a283a455d31
jjs0007 <jjs-0007@outlook.jp>
2014-05-13 12:01:59 +0900

    test

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_03/Exercise5.java
@@ -0,0 +1,20 @@
+package jp.co.jjs.java_seminar.exercise_20140513_03;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        byte x;
+        int y;
+        
+        y = 127;
+        x = (byte)y;
+        System.out.println(x);
+        
+        byte a;
+        int b;
+        
+        b = 258;
+        a = (byte)b;
+        System.out.println(a);
+    }
+
+}
